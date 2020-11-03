3bf3fd463c7651071a371c5d9889fb4df27d7295
Łukasz Andrzejewski <landrzejewski.poczta@gmail.com>
2020-11-03 14:58:27 +0100

    tdd

--- app/src/main/java/pl/training/goodweather/tictactoe/TicTacToe.kt
+++ app/src/main/java/pl/training/goodweather/tictactoe/TicTacToe.kt
@@ -18,20 +18,11 @@ class TicTacToe(crossFields: Set<Int> = mutableSetOf(), circleFields: Set<Int> =
     val isGameOver: Boolean
         get() = allFieldsAreTaken() || playerTookWinningSequence()
 
-    private fun allFieldsAreTaken(): Boolean {
-        return BOARD_SIZE - takenFields().size == 0
-    }
+    private fun allFieldsAreTaken() = BOARD_SIZE - takenFields().size == 0
 
-    private fun takenFields(): Set<Int> {
-        return Stream.concat(crossFields.stream(), circleFields.stream())
-            .collect(Collectors.toSet())
-    }
+    private fun takenFields() =  Stream.concat(crossFields.stream(), circleFields.stream()).collect(Collectors.toSet())
 
-    private fun playerTookWinningSequence(): Boolean {
-        return winningSequences.stream().anyMatch { sequence: Set<Int> ->
-            playerFields().containsAll(sequence)
-        }
-    }
+    private fun playerTookWinningSequence() = winningSequences.stream().anyMatch { playerFields().containsAll(it) }
 
     private fun playerFields(): MutableSet<Int> {
         return if (player === Player.CROSS) crossFields else circleFields
@@ -46,25 +37,22 @@ class TicTacToe(crossFields: Set<Int> = mutableSetOf(), circleFields: Set<Int> =
         return true
     }
 
-    private fun isTaken(field: Int): Boolean {
-        return takenFields().contains(field)
-    }
+    private fun isTaken(field: Int)= takenFields().contains(field)
 
-    fun getCrossFields(): Set<Int> {
-        return Collections.unmodifiableSet(crossFields)
-    }
+    fun getCrossFields(): Set<Int> = Collections.unmodifiableSet(crossFields)
 
-    fun getCircleFields(): Set<Int> {
-        return Collections.unmodifiableSet(circleFields)
-    }
+    fun getCircleFields(): Set<Int> = Collections.unmodifiableSet(circleFields)
 
     companion object {
+
         private const val BOARD_SIZE = 9
-        private val winningSequences = java.util.Set.of(
+
+        private val winningSequences = setOf(
             setOf(1, 2, 3), setOf(4, 5, 6), setOf(7, 8, 9),
             setOf(1, 4, 7), setOf(2, 5, 8), setOf(3, 6, 9),
             setOf(1, 5, 9), setOf(3, 5, 7)
         )
+
     }
 
     init {
