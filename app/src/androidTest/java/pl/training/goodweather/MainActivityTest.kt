ae021305a2c1fe3310c5975538b232fb85a96f6c
Łukasz Andrzejewski <landrzejewski.poczta@gmail.com>
2020-11-04 15:59:52 +0100

    s

--- app/src/androidTest/java/pl/training/goodweather/MainActivityTest.kt
+++ app/src/androidTest/java/pl/training/goodweather/MainActivityTest.kt
@@ -1,39 +1,24 @@
 package pl.training.goodweather
 
 
-import androidx.test.espresso.DataInteraction
-import androidx.test.espresso.ViewInteraction
+import android.view.View
+import android.view.ViewGroup
+import androidx.recyclerview.widget.RecyclerView.ViewHolder
+import androidx.test.espresso.Espresso.onView
+import androidx.test.espresso.action.ViewActions.*
+import androidx.test.espresso.contrib.RecyclerViewActions.actionOnItemAtPosition
+import androidx.test.espresso.matcher.ViewMatchers.*
 import androidx.test.filters.LargeTest
 import androidx.test.rule.ActivityTestRule
 import androidx.test.runner.AndroidJUnit4
-import android.view.View
-import android.view.ViewGroup
-import android.view.ViewParent
-
-import androidx.test.InstrumentationRegistry.getInstrumentation
-import androidx.test.espresso.Espresso.onData
-import androidx.test.espresso.Espresso.onView
-import androidx.test.espresso.Espresso.pressBack
-import androidx.test.espresso.Espresso.openActionBarOverflowOrOptionsMenu
-import androidx.test.espresso.contrib.RecyclerViewActions.actionOnItemAtPosition
-import androidx.recyclerview.widget.RecyclerView.ViewHolder
-import androidx.test.espresso.action.ViewActions.*
-import androidx.test.espresso.assertion.ViewAssertions.*
-import androidx.test.espresso.matcher.ViewMatchers.*
-
-import pl.training.goodweather.R
-
 import org.hamcrest.Description
 import org.hamcrest.Matcher
+import org.hamcrest.Matchers.`is`
+import org.hamcrest.Matchers.allOf
 import org.hamcrest.TypeSafeMatcher
-import org.hamcrest.core.IsInstanceOf
 import org.junit.Rule
 import org.junit.Test
 import org.junit.runner.RunWith
-
-import org.hamcrest.Matchers.allOf
-import org.hamcrest.Matchers.anything
-import org.hamcrest.Matchers.`is`
 
 @LargeTest
 @RunWith(AndroidJUnit4::class)
@@ -46,35 +31,50 @@ class MainActivityTest {
     @Test
     fun mainActivityTest() {
         val appCompatEditText = onView(
-allOf(withId(R.id.city_name),
-childAtPosition(
-childAtPosition(
-withId(R.id.nav_host_fragment),
-0),
-0),
-isDisplayed()))
+            allOf(
+                withId(R.id.city_name),
+                childAtPosition(
+                    childAtPosition(
+                        withId(R.id.nav_host_fragment),
+                        0
+                    ),
+                    0
+                ),
+                isDisplayed()
+            )
+        )
         appCompatEditText.perform(replaceText("warsaw"), closeSoftKeyboard())
-        
+
         val recyclerView = onView(
-allOf(withId(R.id.forecast_list),
-childAtPosition(
-withClassName(`is`("androidx.constraintlayout.widget.ConstraintLayout")),
-1)))
+            allOf(
+                withId(R.id.forecast_list),
+                childAtPosition(
+                    withClassName(`is`("androidx.constraintlayout.widget.ConstraintLayout")),
+                    1
+                )
+            )
+        )
         recyclerView.perform(actionOnItemAtPosition<ViewHolder>(0, click()))
-        
+
         val materialButton = onView(
-allOf(withId(R.id.back_button), withText("Back"),
-childAtPosition(
-childAtPosition(
-withId(R.id.nav_host_fragment),
-0),
-1),
-isDisplayed()))
+            allOf(
+                withId(R.id.back_button), withText("Back"),
+                childAtPosition(
+                    childAtPosition(
+                        withId(R.id.nav_host_fragment),
+                        0
+                    ),
+                    1
+                ),
+                isDisplayed()
+            )
+        )
         materialButton.perform(click())
-        }
-    
+    }
+
     private fun childAtPosition(
-            parentMatcher: Matcher<View>, position: Int): Matcher<View> {
+        parentMatcher: Matcher<View>, position: Int
+    ): Matcher<View> {
 
         return object : TypeSafeMatcher<View>() {
             override fun describeTo(description: Description) {
@@ -89,4 +89,4 @@ isDisplayed()))
             }
         }
     }
-    }
+}
